0a01bdf7cd7dd3d9c41ea895ab4527f35e97b053
Ron Pandolfi <ronpandolfi@gmail.com>
2017-09-13 06:36:45 -0700

    Add doctest to MDFPayload

--- payload_metaclass.py
+++ payload_metaclass.py
@@ -1,5 +1,6 @@
 import datetime
 
+
 class PublishablePayload(dict):
     def __init__(self, title, source_name, data_contact, data_contributor, links, **kwargs):
         """
@@ -8,29 +9,29 @@ class PublishablePayload(dict):
         ----------
         title:              str
         source_name:        str
-        data_contact:       human
-        data_contributor:   human
+        data_contact:       Human
+        data_contributor:   Human
         links:              dict
         """
         super(PublishablePayload, self).__init__()
         self.__dict__ = self
 
-        optionalkeys = ['license',                 # str
-                        'citation',                # list of str
-                        'source_name',             # str
-                        'data_contact',            # human
-                        'data_contributor',        # human
-                        'author',                  # human
-                        'repository',              # str
-                        'collection',              # str
-                        'tags',                    # list of str
-                        'description',             # str
-                        'raw',                     # str
-                        'links',                   # dict
-                        'year',                    # int
-                        'composition']             # str
+        optionalkeys = ['license',  # str
+                        'citation',  # list of str
+                        'source_name',  # str
+                        'data_contact',  # human
+                        'data_contributor',  # human
+                        'author',  # human
+                        'repository',  # str
+                        'collection',  # str
+                        'tags',  # list of str
+                        'description',  # str
+                        'raw',  # str
+                        'links',  # dict
+                        'year',  # int
+                        'composition']  # str
         for prop in optionalkeys:
-            self[prop] = kwargs.get(prop,None)
+            self[prop] = kwargs.get(prop, None)
 
         self['title'] = title
         self['source_name'] = source_name
@@ -46,22 +47,34 @@ class PublishablePayload(dict):
 class CITPayload(PublishablePayload):
     pass
 
+
 class MDFPayload(PublishablePayload):
-    def __init__(self):
-        super(MDFPayload, self).__init__()
+    """
+
+    Examples
+    --------
+    >>> scripty = Human(given_name='Totally', family_name='NotARobot', email='a@a.com', institution='Earth')
+    >>> payload = MDFPayload(title='Test Payload', source_name='Doctest Example Script', data_contact=scripty, data_contributor=scripty, links={'landing_page':'http://www.globus.org'})
+    >>> payload.metapayload
+    {'mdf': {'title': 'Test Payload', 'acl': [], 'source_name': 'Doctest Example Script', 'citation': None, 'links': {'landing_page': 'http://www.globus.org'}, 'data_contact': {}, 'data_contributor': {}, 'ingest_date': 'Sep 13, 2017', 'metadata_version': '1.1', 'mdf_id': '1', 'resource_type': 'dataset'}, 'dc': {}, 'misc': {}}
+
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(MDFPayload, self).__init__(*args, **kwargs)
 
     @property
     def metapayload(self):
         dataset = {
             "mdf": {
                 "title": self.title,
-                "acl": [],                                                      # TODO: what goes here?
+                "acl": [],  # TODO: what goes here?
                 "source_name": self.source_name,
                 "citation": self.citation,
                 "links": self.links,
                 "data_contact": dict(self.data_contact),
                 "data_contributor": dict(self.data_contributor),
-                "ingest_date": datetime.datetime.now().strftime('%b %d, %Y'),   # TODO: shouldn't this be implied?
+                "ingest_date": datetime.datetime.now().strftime('%b %d, %Y'),  # TODO: shouldn't this be implied?
                 "metadata_version": "1.1",
                 "mdf_id": "1",
                 "resource_type": "dataset"
@@ -73,13 +86,11 @@ class MDFPayload(PublishablePayload):
         return dataset
 
 
-
 class MCPayload(PublishablePayload):
     pass
 
 
-class human(dict):
+class Human(dict):
     def __init__(self, given_name, family_name, email='', institution=''):
-        super(human, self).__init__()
+        super(Human, self).__init__()
         self.__dict__ = self
-
